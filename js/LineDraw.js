8c9c466927a9c15bd63ce617d8d1e50800389c7b
zigacer <58218618+zigacer@users.noreply.github.com>
2026-02-05 12:32:20 +0100

    Add files via upload

--- js/LineDraw.js
+++ js/LineDraw.js
@@ -1,9 +1,33 @@
-const path = document.getElementById('crta');
-const length = path.getTotalLength();
+document.addEventListener("DOMContentLoaded", () => {
+  const path = document.getElementById("crta");
+  const length = path.getTotalLength();
+
+  
+  path.style.strokeDasharray = length;
+  path.style.strokeDashoffset = length;
+  path.style.animation = "none";
+});
+const button = document.getElementById("solveButton");
+const path = document.getElementById("crta");
+
+button.addEventListener("click", () => {
+  const length = path.getTotalLength();
+
+
+  path.style.animation = "none";
+  path.style.strokeDasharray = length;
+  path.style.strokeDashoffset = length;
+
+
+  path.getBoundingClientRect();
+
  
-path.style.strokeDasharray = length;
-path.style.strokeDashoffset = length;
- 
-path.getBoundingClientRect();
- 
-path.style.animation = 'draw-line 10s linear forwards';
+  path.style.animation = "draw-line 10s linear forwards";
+
+  
+  path.onanimationend = () => {
+   
+    path.style.animation = "none";
+    path.style.strokeDashoffset = length;
+  };
+});
